3f60c0f605c23de79603f258f8f8fa8a20ab71eb
zengyunlong <273412935@qq.com>
2019-09-27 18:52:10 +0800

    custom eww search prefix

--- init.el
+++ init.el
@@ -577,7 +577,37 @@ dump."
 
 (defun dotspacemacs/user-config ()
 
-  ;; start 配置一下 Emacs Application Framework 
+  ;; start 配置一下 eww
+  (with-eval-after-load 'eww
+    (custom-set-variables
+     ;; '(eww-search-prefix "https://www.google.com.hk/search?q=")
+     '(eww-search-prefix "https://cn.bing.com/search?q=")
+     ;; '(eww-search-prefix "https://www.baidu.com/s?wd=hello")
+     )
+
+    ;; (define-key eww-mode-map (kbd "h") 'backward-char)
+    ;; (define-key eww-mode-map (kbd "n") 'next-line)
+    ;; (define-key eww-mode-map (kbd "s") 'forward-char)
+    ;; (define-key eww-mode-map (kbd "t") 'previous-line)
+
+    ;; (define-key eww-mode-map (kbd "H") 'eww-back-url)
+    ;; (define-key eww-mode-map (kbd "S") 'eww-forward-url)
+
+    ;; (define-key eww-mode-map (kbd "b") 'eww-history-browse)
+    ;; (define-key eww-mode-map (kbd "c") 'eww-browse-with-external-browser)
+    ;; (define-key eww-mode-map (kbd "i") 'eww)
+    ;; (define-key eww-mode-map (kbd "m") 'eww-lnum-follow)
+    ;; (define-key eww-mode-map (kbd "z") 'eww-lnum-universal)
+
+    ;; (define-key eww-mode-map (kbd "M-n") 'nil)
+    ;; (define-key eww-mode-map (kbd "M-p") 'nil)
+
+    ;; (define-key eww-mode-map (kbd "<C-S-iso-lefttab>") 'eww-previous-buffer)
+    ;; (define-key eww-mode-map (kbd "<C-tab>")           'eww-next-buffer)
+    )
+  ;; end 配置一下 eww
+
+  ;; start 配置一下 Emacs Application Framework
   ;; https://github.com/manateelazycat/emacs-application-framework
   ;; https://github.com/Linusp/emacs_config/commit/9b99efa16e8d89ae4fc9563d12a76c7c90613699
   ;; ATTENTION: mac, wsl, can not worked
